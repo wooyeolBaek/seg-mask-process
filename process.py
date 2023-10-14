64e6b5eaaeacf54ff011b4459a31660fadc5a374
wooyeolBaek <dwybaek7@gmail.com>
2023-10-14 17:26:54 +0900

    Fix bfs order

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import heapq
+import argparse
 import concurrent.futures
 from collections import defaultdict, deque
 
@@ -10,16 +11,45 @@ from tqdm import tqdm
 
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="inference")
+
+    parser.add_argument(
+        "--base_path",
+        type=str,
+        default="./masks",
+    )
+    parser.add_argument(
+        "--thr",
+        type=int,
+        default=.1,
+    )
+    parser.add_argument(
+        "--binary",
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "--multiprocess",
+        type=bool,
+        default=True,
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
 def get_regions(mask):
 
     assert len(mask.shape) == 2, AssertionError(f"mask.shape should be 2. not {len(mask.shape)}")
     height, width = mask.shape
 
     visited = np.zeros_like(mask, dtype=bool)
-    result_dict = defaultdict(list)
+    result_list = []
     # dirs = [[-1,0],[0,-1],[0,1],[1,0]]
     dirs = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-    # dirs = [ # 방법 2: 거리 2만큼 탐색
+    # dirs = [
     #     [-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2],
     #     [-1,-2],[-1,-1],[-1,0],[-1,1],[-1,2],
     #     [0,-2],[0,-1],[0,1],[0,2],
@@ -54,13 +84,13 @@ def get_regions(mask):
                             xs.append(nx)
                             ys.append(ny)
                 
-                result_dict[target_value].append((np.array(xs), np.array(ys)))
+                result_list.append([target_value, (np.array(xs), np.array(ys))])
     
     # sort ascending(avoid overlapped small region)
-    for k,v in result_dict.items():
-        result_dict[k].sort(key=lambda x:len(x[0]), reverse=True)    
+    result_list.sort(key=lambda x:len(x[1][0]), reverse=True)
 
-    return result_dict
+    # return result_dict
+    return result_list
 
 
 def get_adj_pixels(region, mask):
@@ -97,20 +127,21 @@ def process(image_dir, save_dir, thr=.1, binary=False):
     mask_name = os.path.basename(image_dir)
     mask = cv2.imread(image_dir, cv2.IMREAD_GRAYSCALE)
 
-    result_dict = get_regions(mask)
+    result_list = get_regions(mask)
 
     nmask = mask.copy()
     h, w = mask.shape
     area = h * w
-    for pixel_value, regions in result_dict.items():
-        for region in regions:
-            if len(region[0]) > area * thr:
-                continue
 
-            npixel_value = get_adj_pixels(region, mask)
-            npixel_value = get_adj_pixels(region, nmask)
+    for pixel_value, region in result_list:
 
-            nmask[region] = npixel_value
+        if len(region[0]) > area * thr:
+            continue
+
+        npixel_value = get_adj_pixels(region, mask)
+        npixel_value = get_adj_pixels(region, nmask)
+
+        nmask[region] = npixel_value
 
     if binary:
         nmask[nmask>0]=255
@@ -119,42 +150,34 @@ def process(image_dir, save_dir, thr=.1, binary=False):
         cv2.imwrite(os.path.join(save_dir, f"{os.path.splitext(mask_name)[0]}.png"),cv2.normalize(nmask, None, 0, 255, cv2.NORM_MINMAX))
 
 
-
-
-
-def main():
-    base_path = "/Users/wooyeolbaek/Documents/Codes/seg-mask-process/masks"
-    thr = .05
-    thr = .1
-    binary = False
-    multiprocess = False
-    multiprocess = True
+def main(args):
 
     # get mask dirs
     mask_dirs = []
     for ext in ["png","jpg","jpeg"]:
-        mask_dirs += glob.glob(base_path + f"/**.{ext}")
+        mask_dirs += glob.glob(args.base_path + f"/**.{ext}")
 
-    base_name = os.path.basename(base_path)
+    base_name = os.path.basename(args.base_path)
     new_name = base_name + "_processed"
-    save_dir = os.path.join(os.path.dirname(base_path), new_name)
+    save_dir = os.path.join(os.path.dirname(args.base_path), new_name)
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
 
-    if multiprocess:
+    if args.multiprocess:
         with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()//2) as executor:
             futures = []
             for mask_dir in mask_dirs:
-                futures.append(executor.submit(process, mask_dir, save_dir, thr, binary))
+                futures.append(executor.submit(process, mask_dir, save_dir, args.thr, args.binary))
 
             print("Processing futures ...")
             for future in tqdm(futures, total=len(futures)):
                 future.result()
     else:
         for mask_dir in tqdm(mask_dirs,total=len(mask_dirs)):
-            process(mask_dir, save_dir, thr, binary)
+            process(mask_dir, save_dir, args.thr, args.binary)
         
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
